a11c0855c18bfcd612b6ecc274f8ffd1d8c6e769
MuGee <jamesjim908@gmail.com>
2023-04-05 08:39:34 -0700

    Updated 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,7 +10,7 @@ void _puts_recursion(char *s) /*uses recursion*/
 	if (*s)
 	{
 		_putchar(*s);
-		_puts_recursion(++s);
+		_puts_recursion(++s); /*Come on Betty*/
 	}
 	else
 		_putchar('\n'); /*a new line*/
