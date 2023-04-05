953f1a7606412527c31266384076500448ec0921
MuGee <jamesjim908@gmail.com>
2023-04-05 08:53:08 -0700

    Updated 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,11 +3,11 @@
 /*
  * _puts_recursion - uses recursion
  * @s: string to recurse
+ * Description: prints a string, followed by a new line.
  */
 
 void _puts_recursion(char *s)
 	/*
-	 * uses recursion
 	 * _puts_recursion - uses recursion
 	 * Description: prints a string, followed by a new line.
 	 */
